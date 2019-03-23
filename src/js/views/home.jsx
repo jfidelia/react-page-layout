251472ddc25e9d2801f4d95e9a132532d5a66a68
Jacques Fidelia <jacques.fidelia@gmail.com>
2019-03-23 17:49:47 +0000

    first commit

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -6,14 +6,115 @@ import "../../styles/home.css";
 export class Home extends React.Component {
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button, bootstrap is working
-				</a>
+			<div className="container">
+				<header className="jumbotron my-4">
+					<h1 className="display-3">A Warm Welcome!</h1>
+					<p className="lead">
+						Lorem ipsum dolor sit amet, consectetur adipisicing
+						elit. Ipsa, ipsam, eligendi, in quo sunt possimus non
+						incidunt odit vero aliquid similique quaerat nam nobis
+						illo aspernatur vitae fugiat numquam repellat.
+					</p>
+					<a href="#" className="btn btn-primary btn-lg">
+						Call to action!
+					</a>
+				</header>
+
+				<div className="row text-center">
+					<div className="col-lg-3 col-md-6 mb-4">
+						<div className="card h-100">
+							<img
+								className="card-img-top"
+								src="http://placehold.it/500x325"
+								alt=""
+							/>
+							<div className="card-body">
+								<h4 className="card-title">Card title</h4>
+								<p className="card-text">
+									Lorem ipsum dolor sit amet, consectetur
+									adipisicing elit. Sapiente esse
+									necessitatibus neque.
+								</p>
+							</div>
+							<div className="card-footer">
+								<a href="#" className="btn btn-primary">
+									Find Out More!
+								</a>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-lg-3 col-md-6 mb-4">
+						<div className="card h-100">
+							<img
+								className="card-img-top"
+								src="http://placehold.it/500x325"
+								alt=""
+							/>
+							<div className="card-body">
+								<h4 className="card-title">Card title</h4>
+								<p className="card-text">
+									Lorem ipsum dolor sit amet, consectetur
+									adipisicing elit. Explicabo magni sapiente,
+									tempore debitis beatae culpa natus
+									architecto.
+								</p>
+							</div>
+							<div className="card-footer">
+								<a href="#" className="btn btn-primary">
+									Find Out More!
+								</a>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-lg-3 col-md-6 mb-4">
+						<div className="card h-100">
+							<img
+								className="card-img-top"
+								src="http://placehold.it/500x325"
+								alt=""
+							/>
+							<div className="card-body">
+								<h4 className="card-title">Card title</h4>
+								<p className="card-text">
+									Lorem ipsum dolor sit amet, consectetur
+									adipisicing elit. Sapiente esse
+									necessitatibus neque.
+								</p>
+							</div>
+							<div className="card-footer">
+								<a href="#" className="btn btn-primary">
+									Find Out More!
+								</a>
+							</div>
+						</div>
+					</div>
+
+					<div className="col-lg-3 col-md-6 mb-4">
+						<div className="card h-100">
+							<img
+								className="card-img-top"
+								src="http://placehold.it/500x325"
+								alt=""
+							/>
+							<div className="card-body">
+								<h4 className="card-title">Card title</h4>
+								<p className="card-text">
+									Lorem ipsum dolor sit amet, consectetur
+									adipisicing elit. Explicabo magni sapiente,
+									tempore debitis beatae culpa natus
+									architecto.
+								</p>
+							</div>
+							<div className="card-footer">
+								<a href="#" className="btn btn-primary">
+									Find Out More!
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
